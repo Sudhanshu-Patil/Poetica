9e5e2f410bed245a44214ce3b5956576ca370185
Sudhanshu Patil <93507588+Sudhanshu-Patil@users.noreply.github.com>
2024-08-27 14:24:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def generate_poem():
     directive = "Write a poem based on the following prompt: "
     full_prompt = f"{directive}{user_prompt}"
     try:
-        genai.configure(api_key="AIzaSyCXFODsArY0ukf3EUU5GVr2cPOwkNHyUpA") #use your api key
+        genai.configure(api_key="GEMINI_API_KEY") #use your api key
         model = genai.GenerativeModel("gemini-1.5-flash")
         # Generate poem using Gemini with prompt
         response = model.generate_content(full_prompt)
